69df240046d45b316bbe0ec3c2b473ae03738d0a
JellyLu <634725546@qq.com>
2023-07-22 16:17:11 +0800

    feat: make section collapsable

--- src/pages/components/section/index.tsx
+++ src/pages/components/section/index.tsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
+import classes from "classnames";
 import Sentence from "../sentence/index";
+
 import styles from "./index.module.scss";
 
 const Section = (
   { name, sentences, index }: SectionType & { index: number }
 ) => {
-  const renderSentences = () => {
-    return sentences.map((sentence, index) => {
-      return <Sentence {...sentence} key={index} index={index} />
-    });
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleClickHeader = () => {
+    setCollapsed(!collapsed);
   };
+
+  const renderSentences = () => {
+    if (collapsed) {
+      return null;
+    }
+
+    return (
+      <div>
+       <div className={styles.divider} />
+       {
+         sentences.map((sentence, index) => {
+           return <Sentence {...sentence} key={index} index={index} />
+         })
+       }
+     </div>
+  );
+  };
+
   return (
     <div className={styles.section}>
-      <div className={styles.title}>No. {index+1} {name}</div>
-      <div className={styles.divider} />
-      <div>
-        {renderSentences()}
+      <div className={styles.header} onClick={handleClickHeader}>
+        <div className={styles.title}>No. {index+1} {name}</div>
+        <div className={classes(styles.icon, collapsed && styles.collapsed)}>^</div>
       </div>
+      {renderSentences()}
     </div>
   );
 };
