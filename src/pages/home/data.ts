394d887865593c8d7d5aafb3c794bc11293b2b77
JellyLu <634725546@qq.com>
2023-07-23 17:03:39 +0800

    feat: add data

--- src/pages/home/data.ts
+++ src/pages/home/data.ts
@@ -7,44 +7,44 @@ const data: SectionType[] = [
         en: "Hello",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "您好，欢迎来到大熊猫的故乡成都",
+        en: "Hello, Welcome to Chengdu, home to the Giant Pandas",
+      },
+      {
+        cn: "欢迎来到成都参与第31届世界大学生夏季运动会",
+        en: "Welcome to the Chengdu 2023 FISU World University Games",
+      },
+      {
+        cn: "祝您在大运村生活愉快",
+        en: "Enjoy your stay in the FISU Games Village",
       },
       {
         cn: "祝您一天愉快",
         en: "Have a nice day",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "祝您比赛好运",
+        en: "Good luck to your competition",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "有什么可以帮您",
+        en: "How can I help you",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "非常感谢",
+        en: "Thank you so much",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "不客气",
+        en: "You are welcome",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "保持联系",
+        en: "Keep in touch",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
-      },
-      {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
-      },
-      {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "再见",
+        en: "See you",
       },
     ],
   },
@@ -52,12 +52,12 @@ const data: SectionType[] = [
     name: "交通",
     sentences: [
       {
-        cn: "你好",
-        en: "Hello",
+        cn: "我可以帮您叫一辆出租车",
+        en: "I can call a taxi for you",
       },
       {
-        cn: "祝您一天愉快",
-        en: "Have a nice day",
+        cn: "祝您旅途愉快",
+        en: "Have a nice trip",
       }
     ],
   },
